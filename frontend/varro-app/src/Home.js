c8a83d4681873e807fc74bf917751b8113a31691
sfkunal <kunal@masagroup.com>
2023-10-15 14:26:54 -0700

    new api

--- frontend/varro-app/src/Home.js
+++ frontend/varro-app/src/Home.js
@@ -25,7 +25,7 @@ function Home() {
     event.preventDefault();
     setIsLoading(true);
 
-    const response = await fetch("http://127.0.0.1:5000/", {
+    const response = await fetch("https://flask-production-cbf0.up.railway.app/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
